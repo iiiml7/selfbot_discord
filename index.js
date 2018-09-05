36070ca2c401af0414f2aec75653ee5df85a0b3b
NoyonneDev <34164412+NoyonneDev@users.noreply.github.com>
2018-09-05 14:04:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var prefix = "$";
+
+client.login("NDg2MjM3NTM3NDMwNjY3Mjk2.Dm_3ig.o1QDFnXEbdL8BWAMj_TfraN5B80");
+
+client.on("ready", () => {
+    console.log("SelfBot is start");
+    client.user.setGame("0011100010101");
+});
+
+client.on ("message", message => {
+    if(message.content === prefix + "help"){
+        if(message.author.id === "343445422909423628"){
+         var help_embed = new Discord.RichEmbed()
+         .setColor("#FFBF00")
+         .setTitle("**Commande disponible**")
+         .addField("prefix", "prefix = $", true)
+         .addField("help", "afficher le panel d'aide", true)
+         .addField("spam", "spam + (message)", true)
+         .setFooter("Help panel")
+         message.channel.sendMessage(help_embed);
+        }
+    }
+});
+
+
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "spam")) {
+        if(message.author.id === "343445422909423628"){
+            let args = message.content.split(" ")
+            args.shift()
+            message.delete();
+            var setIntervalID = setInterval(function(){message.channel.send(args.join(" "))}, 2000);
+            client.on("message", message => {
+                if(message.content.startsWith(prefix + "stop")){
+                    if(message.author.id === "343445422909423628"){
+                        clearInterval(setIntervalID);
+                        message.delete();
+                        message.channel.send("disable");
+                    }
+                }
+            });
+        }
+    }
+});
